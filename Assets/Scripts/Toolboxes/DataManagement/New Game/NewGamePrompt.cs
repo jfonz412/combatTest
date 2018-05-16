d26f76eae5f7368c6348ce89b945628d38eda7a6
James Fonzone <jfonzone@outlook.com>
2018-05-15 20:07:07 -0400

    New game warning panel

---
+++ Assets/Scripts/Toolboxes/DataManagement/New Game/NewGamePrompt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGamePrompt : MonoBehaviour {
+    [SerializeField]
+    private GameObject warningPrompt;
+    private Button button;
+    [SerializeField]
+    private bool activate;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(delegate { warningPrompt.SetActive(activate); });
+    }
+}
